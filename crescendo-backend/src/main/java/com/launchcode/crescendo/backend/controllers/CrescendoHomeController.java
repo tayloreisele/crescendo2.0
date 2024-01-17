504448a96223291092cff3bdfb18543aaa60cbb4
Sarah Metoxen <slmetoxen.com>
2024-01-17 13:18:03 -0600

    Testing if commits show up anywhere without pushing.

--- crescendo-backend/src/main/java/com/launchcode/crescendo/backend/controllers/CrescendoHomeController.java
+++ crescendo-backend/src/main/java/com/launchcode/crescendo/backend/controllers/CrescendoHomeController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class CrescendoHomeController {
     @GetMapping("/")
+    /* @ResponseBody because I'm using text right now */
     @ResponseBody
     public String renderHomePage (Model model){
-        model.addAttribute("headingText", "Crescendo");
+        /* model.addAttribute("headingText", "Crescendo");*/
         return "Crescendo Sign In Page";
     }
 }
