356b1719c191ea0e2bc11411d46d81cee8f5a5f3
Taylor Eisele <taylorreisele@gmail.com>
2024-01-21 12:40:34 -0600

    Added requirements for Sign Up fields.

--- crescendo-frontend/src/Signup.js
+++ crescendo-frontend/src/Signup.js
@@ -16,6 +16,10 @@ class Signup extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    if (this.state.password !== this.state.passwordConfirmation) {
+        alert('Passwords do not match!');
+        return;
+    }
     // Signup logic here
   }
 
@@ -25,17 +29,18 @@ class Signup extends React.Component {
         <form className='signup-form' onSubmit={this.handleSubmit}>
           <label>
             Username <br/>
-            <input type="text" name="username" onChange={this.handleChange} />
+            <input type="text" name="username" onChange={this.handleChange} required minLength="5" />
           </label>
           <br/>
           <label>
             Email <br/>
-            <input type="email" name="email" onChange={this.handleChange} />
+            <input type="email" name="email" onChange={this.handleChange} required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Please enter a valid email address."
+/>
           </label>
           <br/>
           <label>
             Password <br/>
-            <input type="password" name="password" onChange={this.handleChange} />
+            <input type="password" name="password" onChange={this.handleChange} required minLength="8"/>
           </label>
           <br/>
           <label>
