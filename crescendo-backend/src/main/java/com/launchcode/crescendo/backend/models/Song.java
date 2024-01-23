b6bffc0be194d00adaf1802e175e5b9b4b6c2f5b
Sarah Metoxen <slmetoxen.com>
2024-01-23 16:08:42 -0600

    Added imports to build.gradle & application.properties to connect to MySQL.

--- crescendo-backend/src/main/java/com/launchcode/crescendo/backend/models/Song.java
+++ crescendo-backend/src/main/java/com/launchcode/crescendo/backend/models/Song.java
@@ -1,16 +1,17 @@
 package com.launchcode.crescendo.backend.models;
 
-
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-import java.util.Objects;
 
-
+@Entity
 public class Song {
+    @Id /*This is a primary key*/
+    @GeneratedValue /*Allows database to generate the values of our primary key*/
     private int id;
-    private static int nextId = 1;
 
     @NotBlank(message = "Title is required")
     @Size(min = 3, max = 50, message = "Title must be between 3 and 50 characters")
@@ -24,15 +25,11 @@ public class Song {
     private String notes;
 
     public Song(String title, String musician, String notes) {
-        this();
         this.title = title;
         this.musician = musician;
         this.notes = notes;
     }
-    public Song() {
-        this.id = nextId;
-        nextId++;
-    }
+    public Song() {}
 
     public String getTitle() {
         return title;
