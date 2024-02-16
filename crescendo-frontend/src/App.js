02c0bdd51be97721dda0591df1779359c944e946
Sarah Dao <sarahdao@Sarahs-MBP-3.lan>
2024-02-16 15:03:07 -0600

    added fronted API

--- crescendo-frontend/src/App.js
+++ crescendo-frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react';
-import Login from './Login';
-import LandingPage from './LandingPage';
-import Signup from './Signup';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import Login from './Login';
+import Signup from './Signup';
 import Verify from './Verify';
 import Dashboard from './Dashboard';
+import CreateSongForm from './CreateSongForm'; // Import the CreateSongForm component
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path='/verify' element={<Verify />} />
           <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/create-song' element={<CreateSongForm />} /> {/* Add a new route for CreateSongForm */}
         </Routes>
       </div>
     </Router>
