b05b2adde01517504578afe7cf9c960a66e29fed
Sarah Dao <sarahdao@Sarahs-MBP-3.lan>
2024-02-19 19:41:19 -0600

    got the Spotify API back to where it needs to be,still getting a failed to add song message thought

--- crescendo-frontend/src/pages/AddSong.js
+++ crescendo-frontend/src/pages/AddSong.js
@@ -6,7 +6,7 @@ import '../images/EmptyStage.jpg';
 
 const AddSong = () => {
     const navigate = useNavigate();
-    const [song, setSong] = useState({ title:'', musician:'', notes:'', spotifyTrackId:'', file: null });
+    const [song, setSong] = useState({ title: '', musician: '', notes: '', spotifyTrackId: '', file: null });
     const [query, setQuery] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [message, setMessage] = useState('');
@@ -30,7 +30,7 @@ const AddSong = () => {
 
             if (response.status === 200) {
                 setMessage('New Song Added');
-                navigate('/SongTable');
+                navigate('/');
                 setSong({ title: '', musician: '', notes: '', spotifyTrackId: '', file: null });
             } else {
                 setMessage('Failed to add song');
@@ -45,66 +45,83 @@ const AddSong = () => {
         setSong({ ...song, file: event.target.files[0] });
     };
 
-    const handleSearch = async () => {
+    const handleSearch = async (searchTerm) => {
         try {
-            const response = await axios.post('/api/searchSpotify', { query });
-            setSearchResults(response.data);
+            const accessToken = 'BQC-8JTssCLHoNjUudgj6CGr5mEnA27hX711-pT0Xj3QMGSWz6o8cSBtTI974Ouh6-UkbsvN1tzZx0_m8XL5KhcuX2-0qhRUBVG6-ADiuvFSEwNdyLjOaAHxJCa8yDyUvEdNScph2fTR8NMQglWmk8DJGFmrjFpafQi_AUxa0jgculCav0rYVb9ykZRCZCZkbT_Tt3jEmnmexjsFkCSRPkj9PvVQtuB-zNWBAwwsWHcPQaOygSRn9eecyQ';
+            const encodedSearchTerm = encodeURIComponent(searchTerm); // Encode the search term
+            const response = await axios.get('https://api.spotify.com/v1/search', {
+                params: {
+                    q: encodedSearchTerm, // Use the encoded search term
+                    type: 'track'
+                },
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            });
+    
+            const searchResults = response.data.tracks.items.map(item => ({
+                id: item.id,
+                name: `${item.name} - ${item.artists.map(artist => artist.name).join(', ')}`, // Concatenate track name and artists
+                spotifyTrackId: item.id
+            }));
+
+            setSearchResults(searchResults);
         } catch (error) {
             console.error('Error searching Spotify:', error);
         }
     };
 
+    const handleSelectTrack = (spotifyTrackId) => {
+        setSong({ ...song, spotifyTrackId });
+    };
+
     return (
         <div className="background-image-container">
-            <div className="center-container"> 
-                <div className="song-container-big"> 
-                    <div className="newSong-div">
-                        <h2>Create New Song</h2>
-                        <form className="newSongForm" onSubmit={handleSubmit}>
-                            <input
-                                type="text"
-                                value={song.title}
-                                onChange={(e) => setSong({ ...song, title: e.target.value })}
-                                placeholder="Title"
-                                required
-                            />
-                            <input
-                                type="text"
-                                value={song.musician}
-                                onChange={(e) => setSong({ ...song, musician: e.target.value })}
-                                placeholder="Musician/Show"
-                                required
-                            />
-                            <input
-                                type="text"
-                                value={query}
-                                onChange={(e) => setQuery(e.target.value)}
-                                placeholder="Search Spotify"
-                            />
-                            <button type="button" onClick={handleSearch}>Search</button>
-                            <ul>
-                                {searchResults.map((result) => (
-                                    <li key={result.id}>
-                                        {result.name} - {result.artist}
-                                        <button type="button" onClick={() => setSong({ ...song, spotifyTrackId: result.id })}>Select</button>
-                                    </li>
-                                ))}
-                            </ul>
-                            <input
-                                type="file"
-                                onChange={handleFileChange}
-                                required
-                            />
-                            <input
-                                type="text"
-                                value={song.notes}
-                                onChange={(e) => setSong({ ...song, notes: e.target.value })}
-                                placeholder="Notes"
-                            />
-                            <button type="submit" className='create-button'>Create</button>
-                        </form>
-                        {message && <p>{message}</p>}
-                    </div>
+            <div className="song-container-big">
+                <div className="newSong-div">
+                    <h2>Create New Song</h2>
+                    <form className="newSongForm" onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            value={song.title}
+                            onChange={(e) => setSong({ ...song, title: e.target.value })}
+                            placeholder="Title"
+                            required
+                        />
+                        <input
+                            type="text"
+                            value={song.musician}
+                            onChange={(e) => setSong({ ...song, musician: e.target.value })}
+                            placeholder="Musician/Show"
+                            required
+                        />
+                        <input
+                            type="text"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                            placeholder="Search Spotify"
+                        />
+                        <button type="button" onClick={() => handleSearch(query)}>Search</button>
+                        <select value={song.spotifyTrackId} onChange={(e) => setSong({ ...song, spotifyTrackId: e.target.value })}>
+                            <option value="">Select Track</option>
+                            {searchResults.map(result => (
+                                <option key={result.id} value={result.spotifyTrackId}>{result.name}</option>
+                            ))}
+                        </select>
+                        <input
+                            type="file"
+                            onChange={handleFileChange}
+                            required
+                        />
+                        <input
+                            type="text"
+                            value={song.notes}
+                            onChange={(e) => setSong({ ...song, notes: e.target.value })}
+                            placeholder="Notes"
+                        />
+                        <button type="submit" className='create-button'>Create</button>
+                    </form>
+                    {message && <p>{message}</p>}
                 </div>
             </div>
         </div>
